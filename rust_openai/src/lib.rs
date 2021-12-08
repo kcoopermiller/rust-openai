a88257e6a1ac50f12ff2f6dede7f3fa4c393ebd5
Cooper <kcoopermiller9@gmail.com>
2021-12-08 14:25:32 -0600

    removed unused methods

--- rust_openai/src/lib.rs
+++ rust_openai/src/lib.rs
@@ -1,10 +1,7 @@
 // type Result<T> = std::result::Result<T, Error>;
 
 pub mod openai {
-    // sending http requests
     use reqwest::header::{HeaderMap, HeaderValue, AUTHORIZATION, CONTENT_TYPE};
-
-    // (de)serialize json
     use serde::{Deserialize, Serialize};
 
     #[derive(Debug, Deserialize)]
@@ -18,12 +15,6 @@ pub mod openai {
         object: String,
         owner: String,
         ready: bool,
-    }
-
-    #[derive(Debug, Clone)]
-    pub struct Client {
-        _api_key: String,
-        _base_url: String,
     }
 
     #[derive(Debug, Deserialize)]
@@ -44,9 +35,21 @@ pub mod openai {
     }
 
     #[derive(Serialize, Deserialize, Debug)]
-    struct Body {
+    struct Body { // make a default option
         prompt: String,
         max_tokens: i32,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    struct FinetuneBody {
+        _training_file: String,
+        _base_model: String,
+    }
+
+    #[derive(Debug, Clone)]
+    pub struct Client {
+        _api_key: String,
+        _base_url: String,
     }
 
     impl Client {
@@ -78,7 +81,26 @@ pub mod openai {
             Ok(())
         }
 
-        pub async fn engine_info() {}
+        pub async fn engine(&self, engine_id: &str) -> Result<(), Box<dyn std::error::Error>> {
+            let client = reqwest::Client::new();
+            let mut headers = HeaderMap::new();
+            headers.insert(
+                AUTHORIZATION,
+                format!("Bearer {}", &self._api_key)
+                    .parse::<HeaderValue>()
+                    .unwrap(),
+            );
+            let resp = client
+                .get(format!("{}/engines/{}", &self._base_url, &engine_id))
+                .headers(headers)
+                .send()
+                .await?;
+
+            // deserializing into Engine
+            let json = resp.json::<Engine>().await?;
+            println!("{:#?}", json);
+            Ok(())
+        }
 
         pub async fn completion(
             &self,
@@ -107,8 +129,7 @@ pub mod openai {
             let resp = client
                 .post(format!(
                     "{}/engines/{}/completions",
-                    &self._base_url,
-                    engine_id.to_string()
+                    &self._base_url, &engine_id
                 ))
                 .headers(headers)
                 .json(&params)
@@ -121,8 +142,37 @@ pub mod openai {
             Ok(())
         }
 
-        pub async fn search() {}
+        pub async fn finetune(&self, training_file: &str,  base_model: &str) -> Result<(), Box<dyn std::error::Error>> {
+            let mut headers = HeaderMap::new();
+            headers.insert(
+                CONTENT_TYPE,
+                "application/json".parse::<HeaderValue>().unwrap(),
+            );
+            headers.insert(
+                AUTHORIZATION,
+                format!("Bearer {}", &self._api_key)
+                    .parse::<HeaderValue>()
+                    .unwrap(),
+            );
+            
+            let params = FinetuneBody {
+                _training_file: training_file.to_string(),
+                _base_model: base_model.to_string()
+            };
 
-        pub async fn finetune() {}
+            let client = reqwest::Client::new();
+            let resp = client
+                .post(" 
+                https://api.openai.com/v1/fine-tunes")
+                .headers(headers)
+                .json(&params)
+                .send()
+                .await?;
+
+            let json = resp.json::<PostResponse>().await?;
+
+            println!("{:#?}", json);
+            Ok(())
+        }
     }
 }
